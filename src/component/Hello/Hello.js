5194c173b9e8e3edc51d8779bd3ca2e19001b99a
wb-wjy286727 <wb-wjy286727@alibaba-inc.com>
2018-08-06 17:59:21 +0800

    create

---
+++ src/component/Hello/Hello.js
@@ -0,0 +1,15 @@
+/**
+ * @fileOverview
+ * @author ISS
+ */
+import React, {Component} from 'react';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello,React!
+      </div>
+    )
+  }
+}
---
+++ src/pages/Page1/Page1.js
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview
+ * @author ISS
+ */
+
+import React, {Component} from 'react';
+
+export default class Page1 extends Component {
+  render() {
+    return (
+      <div>
+        this is Page~
+      </div>
+    )
+  }
+}
